75f240a0d1e22d6bdd2c7505fbc5566fb04387dc
sophiephill <sp8268@gmail.com>
2025-03-19 13:31:33 -0700

    scripts to make data summary and log rank tables

---
+++ r/src/logrank_tests.R
@@ -0,0 +1,54 @@
+
+setwd("~/Desktop/coursework/biostat215/SurvivalProjW25/r")
+library(survMisc)
+library(gtsummary)
+library(gt)
+
+mypal <- c("#1F77B4", "#FF7F0E", "#2CA02C", "#D62728")
+mypal2 <- c("#17BECF", "#BCBD22", "#FF6347", "#6A5ACD")
+
+data <- (read.csv("data/survDataCleaned.csv")
+         %>% mutate(time_to_death_years = time / 365,
+                    race = factor(race_cleaned), 
+                    FIGO = factor(FIGO)))
+head(data)
+
+###### Compare survival by FIGO stage
+fit <- survfit(Surv(time/365, delta) ~ FIGO, data = data, conf.type = 'none')
+plot(fit, col = mypal, years = "Time (years)", ylab = "S(t)")
+legend("bottomleft", col = mypal, lty = 1, bty = "n", cex = 1.2,
+       legend = levels(data$FIGO))
+
+logranks <- ten(Surv(time, delta) ~ FIGO, data = data) 
+comp(logranks, p = c(1, 0), q = c(0, 1))
+tbl <- data.frame(attr(logranks, "tft")$tft)
+tbl <- tbl[,c(1,2,6,8)]
+colnames(tbl) <- c("Test", "Z(t)", "Chi-square", "p-value")
+tbl[,-1] <- round(tbl[,-1], 2)
+tbl$Test <- c("Log-rank", "Gehan", "Tarone-Ware", "Peto-Peto", 
+              "Modified PP","FH p=1,q=0", "FH p=0,q=1")
+gt(tbl) %>% tab_style(
+  style = cell_text(weight = "bold"),  # Make column headers bold
+  locations = cells_column_labels()
+)
+
+
+###### Compare survival by race
+fit <- survfit(Surv(time/365, delta) ~ race, data = data %>% filter(!grepl("unreported", race)), conf.type = 'none')
+plot(fit, col = mypal2, xlab = "Time (years)", ylab = "S(t)", cex.lab = 1.2)
+legend("bottomleft", col = mypal2, lty = 1, lwd =2,  bty = "n", cex = 1.2,
+       legend = levels(data$race)[-4])
+
+
+logranks <- ten(Surv(time, delta) ~ race, data = data %>% filter(!grepl("unreported", race)))
+comp(logranks, p = c(1, 0), q = c(0, 1))
+tbl <- data.frame(attr(logranks, "tft")$tft)
+tbl <- tbl[,c(1,2,6,8)]
+colnames(tbl) <- c("Test", "Z(t)", "Chi-square", "p-value")
+tbl[,-1] <- round(tbl[,-1], 2)
+tbl$Test <- c("Log-rank", "Gehan", "Tarone-Ware", "Peto-Peto", 
+              "Modified PP","FH p=1,q=0", "FH p=0,q=1")
+gt(tbl) %>% tab_style(
+  style = cell_text(weight = "bold"),  # Make column headers bold
+  locations = cells_column_labels()
+)
---
+++ r/src/make_tbl1_summary.R
@@ -0,0 +1,31 @@
+
+setwd("~/Desktop/coursework/biostat215/SurvivalProjW25/r")
+library(gtsummary)
+
+
+data <- (read.csv("data/survDataCleaned.csv")
+         %>% mutate(time_to_death_years = time / 365,
+                    Race = factor(race_cleaned), 
+                    FIGO = factor(FIGO),
+                    delta = ifelse(delta == 1, "dead", "alive"))
+         %>% rename(`Survival time (years)` = time_to_death_years,
+                    `Size of biopsy (cm)` =  size.intermediate,
+                    `Age at diagnosis` = age_at_diagnosis,
+                    `Vital status` = delta,
+                    `FIGO stage` = FIGO))
+
+head(data)
+
+
+vars <- c("Survival time (years)", "Size of biopsy (cm)", "Age at diagnosis", "Race", "FIGO stage")
+factorVars <- c( "Race", "FIGO stage")
+
+table1 <- (data[,c(vars, "Vital status")] %>% tbl_summary(by = `Vital status`)
+           %>% add_p()  # Add p-values
+           %>% modify_header(label = "**Variable**") %>% bold_labels() )
+
+table1
+
+
+
+
